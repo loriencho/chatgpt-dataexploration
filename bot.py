d5480ea836f184d5e096603483f416b90a9f507a
Lorien Cho <lorien@instance-1.us-central1-a.c.chatgpt-test-391116.internal>
2023-08-21 20:53:26 +0000

    Tokens are now displayed + extra bug fifxes

--- bot.py
+++ bot.py
@@ -8,7 +8,8 @@ app = App(token=SLACK_BOT_TOKEN)
 global dataframes, output
 dataframes = {}
 
-template = """Answer questions about data in Pandas dataframes stored in a dictionary called 'dataframes' with each key corresponding to the name of a dataframe and the value being the dataframe itself. Generate Python code to be executed on the requested dataframe(s) to answer these questions in a print statement. If printing a dataframe, first put it in csv format. Use markdown to notate all generated code blocks.
+template = """Answer questions about Pandas dataframes (previously read from csv files) stored in a dictionary called dataframes. Sample data will be provided for each dataframe as well as information on its dictionary key. Generate Python code to be executed on the requested dataframe(s) to answer these questions in a print statement. Print in csv format for all tabular data, especially dataframes. Use markdown to notate all generated code blocks. Be as concise as possible in responses.
+The user will sometimes provide sample data for a dataframe or code output to you. You do not need to generate code- just give a short acknowledgment.
 
 Current conversation:
 {history}
@@ -16,7 +17,7 @@ Current conversation:
 Human: {input}
 AI:"""
 
-memory = ConversationBufferWindowMemory(k=4)
+memory = ConversationBufferMemory()
 PROMPT = PromptTemplate(input_variables=["history", "input"], template=template)
 model = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613")
 chain = ConversationChain(
@@ -31,7 +32,6 @@ chain = ConversationChain(
 
 global output
 output = ""
-
 @app.command("/csv")
 def upload_csv(ack: Ack, respond : Respond, command : dict, client: WebClient):
     global dataframes
@@ -40,60 +40,82 @@ def upload_csv(ack: Ack, respond : Respond, command : dict, client: WebClient):
     last_message = client.conversations_history(channel=command["channel_id"])["messages"][0]
     
     if ("files" not in last_message or len(last_message["files"])==0):
-        client.chat_postMessage(text="No file upload in last message", channel=channel_id)
+        respond("No file upload in last message")
         return
 
     try:
         
         # Find file(s) from last message sent
-        name = command["text"].strip()
+        names = command["text"].strip().split(",")
         files = last_message["files"]
-        uploaded = None
-        for i in range(len(files)):
-            if (files[i]["title"] == name):
-                uploaded = files[i]
-        if uploaded == None:
-            respond("File {name} not found")
-            return
-        url = uploaded["url_private"]
-        
-        # Get csv files
-        processing_res = client.chat_postMessage(text="Downloading csv files...", channel=channel_id)
-        res = requests.get(url, headers={'Authorization': 'Bearer %s' % SLACK_BOT_TOKEN})
-        res.raise_for_status()
-        csv = res.content     
-        
-        # Add to dataframes dictionary
-        filename = name.split(".")[0]
-        processing_res = client.chat_postMessage(text="Reading csv file...", channel=channel_id)
-        df = pd.read_csv(BytesIO(csv)) 
-        dataframes[filename] = df
-        
-        # Feed sample data
-        client.chat_update(text="Processing sample data...", channel=channel_id, ts=processing_res["ts"])
-        lines = res.text.split("\n")
-        sample_lines = len(lines) if len(lines)<4 else 4
-        sample_data = lines[:sample_lines]       
-        chain.predict(input=f"Sample data for the {filename} dataframe:\n {sample_data}")
-        client.chat_update(text=f"Successfully processed CSV file {name}", channel=channel_id, ts=processing_res["ts"])
 
-    except:
-        respond("Error reading CSV file")
+        for name in names:
+            name = name.strip()
+            filename = name.split(".")[0]
+    
+            uploaded = None
+            for i in range(len(files)):
+                if (files[i]["title"] == name):
+                    uploaded = files[i]
+            if uploaded == None:
+                respond(f"File {name} not found")
+                continue
+            if filename in dataframes:
+                client.chat_postMessage(text=f"File {filename} already exists and will not be downloaded.", channel=channel_id)
+                continue
+            url = uploaded["url_private"]
+        
+            # Get csv files
+            processing_res = client.chat_postMessage(text=f"Downloading csv file {name}...", channel=channel_id)
+            res = requests.get(url, headers={'Authorization': 'Bearer %s' % SLACK_BOT_TOKEN})
+            res.raise_for_status()
+            csv = res.content     
+        
+            # Add to dataframes dictionary
+            processing_res = client.chat_update(text=f"Reading csv file {name}...", channel=channel_id, ts=processing_res["ts"])
+            df = pd.read_csv(BytesIO(csv)) 
+            dataframes[filename] = df
+        
+            # Feed sample data
+            client.chat_update(text=f"Processing sample data for {name}...", channel=channel_id, ts=processing_res["ts"])
+            lines = res.text.split("\n")
+            sample_lines = len(lines) if len(lines)<3 else 3
+            sample_data = lines[:sample_lines]       
+
+            try:
+                with get_openai_callback() as cb:
+                    chain.predict(input=f"Sample data for the \"{filename}\" dataframe stored in dataframes[\"{filename}\"]:\n {sample_data}")
+                    client.chat_update(text=f"Successfully processed CSV file {name}. Current token usage: {cb.total_tokens}", channel=channel_id, ts=processing_res["ts"])
+
+            except Exception as error:
+                client.chat_update(text=f"An error occurred when giving {filename} sample data to ChatGPT: {type(error).__name__}- {error}", channel=channel_id, ts=processing_res["ts"])
+
+    except Exception as error:
+        respond(f"Error reading CSV file- {type(error).__name__} - {error}")
 
 @app.command("/say")
 def bot_input(ack: Ack, respond : Respond, command: dict, client: WebClient):
     global output
     ack()
 
-    if csv == None:
-        client.chat_postMessage(text="Please set a csv file.", channel=command["channel_id"])
+    channel_id = command["channel_id"]
+
+    if len(dataframes) == 0:
+        respond("Please set a csv file.")
 
     # Send user input to ChatGPT
-    res = client.chat_postMessage(text="Processing your input...", channel=command["channel_id"])
+    res = client.chat_postMessage(text="Processing your input...", channel=channel_id)
     prompt = command["text"]
-    output = chain.predict(input=prompt)
-    client.chat_update(text=f"Finished processing: \n{prompt}", channel=command["channel_id"], ts=res["ts"])
-    client.chat_postMessage(text=f"{output}", channel=command["channel_id"]) 
+
+    try:
+        with get_openai_callback() as cb:
+            output = chain.predict(input=prompt)
+            client.chat_update(text=f"Finished processing: \n{prompt}.", channel=channel_id, ts=res["ts"])
+            client.chat_postMessage(text=f"{output}\nCurrent tokens: {cb.total_tokens}", channel=channel_id) 
+
+    except Exception as error:
+        client.chat_postMessage(text=f"An error occurred when sending your input to ChatGPT: {type(error).__name__}: {error}", channel=channel_id)
+        
 
 @app.command("/clear")
 def clear(ack: Ack, respond: Respond, command:dict, client: WebClient):
@@ -133,7 +155,7 @@ def run_code_output(ack: Ack, respond : Respond, command:dict, client : WebClien
     # Get code from the last message
     split = output.split('```python')
     if len(split) <= 1:
-        client.chat_postMessage(text=f"No python code generated", channel=channel_id)
+        respond("No python code generated.")
         return 
     output_aftercode = split[1]
     output_code = output_aftercode.split('```')[0]
@@ -150,23 +172,44 @@ def run_code_output(ack: Ack, respond : Respond, command:dict, client : WebClien
     except Exception as error:
         sys.stdout = tmp
         client.chat_postMessage(
-                text=f"There was an error running the code: \n {type(error).__name__}", channel=channel_id)
+                text=f"There was an error running the code: \n {type(error).__name__}: {error}", channel=channel_id)
         return
-
-    # Feed output to ChatGPT
-    chain.save_context({"input":f"I ran the code. Output: \"{output.getvalue()}\"", "output":"Got it."})
     
+    # Feed output to ChatGPT
+    output = output.getvalue()
+    orig_output = output
+    output_lines = output.split("\n")
+
+    if len(output_lines) > 3:
+        respond("Output was too long to send completely, and a sample was sent to ChatGPT.")
+        tmp = "I ran the code, here's a sample of the code output: "
+        for i in range(3):
+            tmp += output_lines[i]
+        output = tmp
+    else:
+        output = f"I ran the code, here's the output: {output}"
+    try:
+        with get_openai_callback() as cb:
+            chain.predict(input=output)
+            respond(f"Sent output, total tokens are {cb.total_tokens}")
+    except Exception as error:
+        client.chat_postMessage(text=f"There was an error sending the output to ChatGPT: {type(error).__name__} - {error}", channel=channel_id)
+        
     # Send results file to user.
     filename = command["text"].strip() # check if user provided a filename
     if filename == "":
         filename = "results.txt"
-    processing_res = client.chat_postMessage(text="Uploading output file...", channel=channel_id)
-    res = client.files_upload(
-        filename=filename,
-        content=output.getvalue(),
-        channels=channel_id)
-    client.chat_update(text="Output file sent.", channel=channel_id, ts=processing_res["ts"])
-    
+
+    if len(orig_output)!=0:
+        processing_res = client.chat_postMessage(text="Uploading output file...", channel=channel_id)
+        res = client.files_upload(
+            filename=filename,
+            title=filename,
+            content=orig_output,
+            channels=channel_id)
+        client.chat_update(text="Output file sent.", channel=channel_id, ts=processing_res["ts"])
+    else:
+        respond("No output from code.")
 def main():
     handler = SocketModeHandler(app, SLACK_APP_TOKEN).start()
 
--- dependencies.py
+++ dependencies.py
@@ -2,50 +2,30 @@ import os
 import sys
 import psutil 
 
-from tabulate import tabulate
-import asyncio
-import datetime
-import time 
 from io import BytesIO, StringIO
 import requests
-from langchain.docstore.document import Document
+
 ####
 # Slackbot Imports
 ####
 import slack_sdk as slack
 from slack_sdk import WebClient
-from slack_sdk.web.async_client import AsyncWebClient
 
 from slack_bolt import App, Ack, Respond
-from slack_bolt.async_app import AsyncApp
 from slack_bolt.adapter.socket_mode import SocketModeHandler
-from slack_bolt.adapter.socket_mode.aiohttp import AsyncSocketModeHandler
 
 ####
 # LangChain Imports
 ####
 from langchain import OpenAI
-
-# Use PandasDataframeAgent
+from langchain.callbacks import get_openai_callback
+# Use pandas dataframes
 import pandas as pd
 from pandas import DataFrame
-from langchain.agents import create_pandas_dataframe_agent
-from langchain.agents.agent_types import AgentType
 
 # Use ChatGPT conversationally with context (data)
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationChain
-from langchain.memory import ConversationBufferWindowMemory
+from langchain.memory import ConversationBufferMemory
 from langchain.prompts.prompt import PromptTemplate
-from langchain.schema import(
-        AIMessage,
-        HumanMessage,
-        SystemMessage)
-
-# Use RetrievalQA Chain with chunking
-from langchain.chains import RetrievalQA
-from langchain.document_loaders import CSVLoader
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import Chroma
 
